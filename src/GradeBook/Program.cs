2d3a7f12e5d07b052ce9dca41215b3ca252b8ad1
Troy Jones <sntnmjones@gmail.com>
2020-11-15 12:12:22 -0800

    Looping through arrays
    Intro to var and foreach

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,14 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new[] {1.1, 2.0, 3.0, 4.0};
+            var sum = 0.0;
+            foreach(var number in numbers)
+            {
+                sum += number;
+            }
+            System.Console.WriteLine(sum);
+
             if (args.Length > 0) 
             {
                 Console.WriteLine($"Hello {args[0]}!");
